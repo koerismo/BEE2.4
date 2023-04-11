5ac29c2facbe8c15b986027e884a2c658a51e6e3
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2023-04-11 23:44:22 +0200

    declare class fields and do not overide static method
    ~ Firm

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -1,5 +1,5 @@
-''' ui/__init__.py
-	This file serves as the root for all UI-related code. '''
+""" ui/__init__.pyi
+	This file serves as the root for all UI-related code. """
 
 # Qt UI framework
 from PySide6.QtWidgets import QApplication
@@ -8,19 +8,18 @@ from .backend import BeeGenericBackend
 from .windows.main import BeeWindowMain
 from .windows.export import BeeWindowExport
 
+
 class BeeApplication(QApplication):
-	''' Connects the active backend to a new interface instances. '''
+	""" Connects the active backend to a new interface instances. """
+	window_main: BeeWindowMain
+	window_export: BeeWindowExport
 
 	def __init__(self, backend: BeeGenericBackend):
 		super().__init__()
 		self.backend = backend
-
-	def exec(self) -> int:
 		self.window_main = BeeWindowMain(self.backend)
 		self.window_main.show()
 
 		self.window_export = BeeWindowExport(self.backend, self.window_main)
 		self.window_export.setGeometry(self.window_main.x() + self.window_main.width() + 10, self.window_main.y() + 31, self.window_export.width(), self.window_export.height())
 		self.window_export.show()
-
-		return super().exec()
