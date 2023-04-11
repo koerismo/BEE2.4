5a9a5ed31dbd7a3e239fe46bde48719f681ffba9
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2023-04-11 23:42:36 +0200

    dummy is now dummy
    ~ Firm

--- src/ui/__main__.py
+++ src/ui/__main__.py
@@ -1,13 +1,26 @@
-''' ui/__main__.py
-	This file serves as a test suite for UI operations with a dummy backend. '''
+""" ui/__main__.py
+	This file serves as a test suite for UI operations with a dummy backend. """
 
 from . import BeeApplication, BeeGenericBackend
 
+
 # Create dummy API
-class BeeDummyBackend(BeeGenericBackend): pass
+class BeeDummyBackend(BeeGenericBackend):
+	def version(self) -> str:
+		return 'dummy'
+
+	def read_config(self) -> dict:
+		return { }
+
+	def write_config(self, cfg: dict) -> None:
+		...
+
+	def read_packages(self):
+		...
+
 
 # Start app
 test_backend = BeeDummyBackend()
 test_app = BeeApplication(backend=test_backend)
 # test_app.setStyle('fusion')
-test_app.exec()
+BeeApplication.exec()
